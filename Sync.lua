ad4a10a9c31ce0bfdceef2ce5bb2e3a7031653a9
jlelic <joseph.lelic@gmail.com>
2022-10-22 02:20:17 +0200

    Initial commit

---
+++ Sync.lua
@@ -0,0 +1,381 @@
+local meta
+
+local PROTOCOL_VERSION = '0'
+local PREFIX = 'WCD'
+local UPDATE_INTERVAL = 5
+local DELIMITER = ';'
+local COMPRESSION_LEVEL = 5
+local MSG_HELLO = 'HELLO'
+local MSG_IM_BOSS = 'IMBOSS'
+local MSG_HELLOBACK = 'HELLOBACK'
+local MSG_REQUEST = 'REQUEST'
+local MSG_RECIPES = 'R'
+local MSG_BYE = 'BYE'
+local BOSS_TTL = 5
+
+local aceComm
+
+local imBoss = true
+local waitingForBoss = true
+
+local outOfDatMentioned = false
+
+local playerName = UnitName('player')
+local addonUsers = {playerName}
+local bossName
+
+local b64toNum = {}
+local chars = {}
+for b64code, char in pairs {
+    [0] = 'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+    '0',
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+    '+',
+    '/',
+    '='
+} do
+    b64toNum[strbyte(char, 1)] = b64code
+    chars[b64code] = char
+end
+
+local professionIdToName = {
+    A = 'Alchemy',
+    B = 'Blacksmithing',
+    C = 'Cooking',
+    E = 'Enchanting',
+    G = 'Engineering',
+    H = 'Herbalism',
+    I = 'Inscription',
+    J = 'Jewelcrafting',
+    L = 'Leatherworking',
+    M = 'Mining',
+    S = 'Skinning',
+    T = 'Tailoring'
+}
+
+local firstEvent
+local lastEvent
+
+local function enqueueEvent(data)
+    local event = {data = data}
+    if not firstEvent then
+        firstEvent = event
+    else
+        lastEvent.next = event
+    end
+    lastEvent = event
+end
+
+local function popNextEvent()
+    if not firstEvent then return nil end
+    local result = firstEvent
+    firstEvent = firstEvent.next
+    return result.data
+end
+
+local function professionNameToId(professionName)
+    local id = strsub(professionName, 1, 1)
+    if professionName == 'Engineering' then id = 'G' end
+    return id
+end
+
+local function getSortedKeys(professionName)
+    local keys = {}
+    for key, _ in pairs(WhoCanDo.Database[professionName]) do tinsert(keys, key) end
+    sort(keys)
+    return keys
+end
+
+local function buildMessage(type, data) return PROTOCOL_VERSION .. DELIMITER .. type .. DELIMITER .. data end
+
+local function compressGuildMetaData()
+    local result = {}
+    for professionName, profsData in pairs(WCDMetaData) do
+        local subresult = {}
+        if WhoCanDo.Database[professionName] then
+            for name, total in pairs(profsData) do
+                if tContains(WhoCanDo.Guildies, name) then tinsert(subresult, name .. '-' .. total) end
+            end
+            local profString = professionNameToId(professionName) .. ':' .. table.concat(subresult, ',')
+            tinsert(result, profString)
+        end
+    end
+    local rawData = table.concat(result, DELIMITER)
+    local compressed = deflate:EncodeForWoWAddonChannel(deflate:CompressDeflate(rawData, {level = COMPRESSION_LEVEL}))
+    return compressed
+end
+
+local function sendHello()
+    local msg = buildMessage(MSG_HELLO, compressGuildMetaData())
+    aceComm:SendCommMessage(PREFIX, msg, 'GUILD', nil, 'BULK')
+end
+
+local function sendHelloBack(target)
+    local msg = buildMessage(MSG_HELLOBACK, playerName)
+    aceComm:SendCommMessage(PREFIX, msg, 'WHISPER', target, 'ALERT')
+end
+
+local function sendImBoss()
+    imBoss = true
+    waitingForBoss = false
+    bossName = playerName
+    local msg = buildMessage(MSG_IM_BOSS, playerName)
+    aceComm:SendCommMessage(PREFIX, msg, 'GUILD', nil, 'ALERT')
+end
+
+local function sendRequest(target, professionName, name)
+    local msg = buildMessage(MSG_REQUEST, professionNameToId(professionName) .. ',' .. name)
+    aceComm:SendCommMessage(PREFIX, msg, 'WHISPER', target, 'NORMAL')
+end
+
+local function sendRecipes(target, professionName, name)
+    local sortedKeys = getSortedKeys(professionName)
+    local data = {}
+    local modStep = 0
+    local currentValue = 0
+
+    -- Pad the list of keys to be divisible by 6
+    -- so we can correctly decode last char
+    while #sortedKeys % 6 ~= 0 do tinsert(sortedKeys, 0) end
+
+    for _, key in ipairs(sortedKeys) do
+        local knows = WCDData[key] and tContains(WCDData[key], name)
+        if knows then currentValue = currentValue + 1 end
+        modStep = modStep + 1
+        if modStep == 6 then
+            tinsert(data, chars[currentValue])
+            currentValue = 0
+            modStep = 0
+        else
+            currentValue = currentValue * 2
+        end
+    end
+    local strData = table.concat(data, '')
+    local profId = professionNameToId(professionName)
+    msgData = profId .. ',' .. name .. ',' .. strData
+    local msg = buildMessage(MSG_RECIPES, msgData)
+    local channel = target == 'GUILD' and 'GUILD' or 'WHISPER'
+    aceComm:SendCommMessage(PREFIX, msg, channel, target, 'NORMAL')
+end
+
+local function updateRecipesFromMessage(message)
+    local profId, name, data = strsplit(',', message)
+    local professionName = professionIdToName[profId]
+    local sortedKeys = getSortedKeys(professionName)
+    for i = 0, (strlen(data) - 1) do
+        local num = b64toNum[strbyte(data, i + 1)]
+        for j = 5, 0, -1 do
+            local spellId = sortedKeys[i * 6 + j + 1]
+            if num % 2 == 1 then
+                WhoCanDo:AddRecipe(professionName, spellId, name)
+                num = num - 1
+            end
+            num = num / 2
+        end
+    end
+end
+
+local function compareAndSync(sender, compressedMsg)
+    local data = deflate:DecompressDeflate(deflate:DecodeForWoWAddonChannel(compressedMsg), {level = COMPRESSION_LEVEL})
+    local profsData = {strsplit(DELIMITER, data)}
+    WhoCanDo:DebugLog('Compare and sync with ' .. sender)
+    for _, profData in ipairs(profsData) do
+        local profId, charactersData = strsplit(':', profData)
+        if charactersData then
+            local professionName = professionIdToName[profId]
+            local entries = {strsplit(',', charactersData)}
+            for _, entry in ipairs(entries) do
+                local name, strOtherTotal = strsplit('-', entry)
+                local otherTotal = tonumber(strOtherTotal)
+                local ownTotal = (WCDMetaData[professionName] or {[name] = 0})[name]
+
+                if ownTotal < otherTotal then
+                    enqueueEvent({type = MSG_REQUEST, name = name, profession = professionName, target = sender})
+                elseif ownTotal > otherTotal then
+                    if imBoss then
+                        enqueueEvent({type = MSG_RECIPES, name = name, profession = professionName, target = sender})
+                    end
+                else
+                    WhoCanDo:DebugLog(ownTotal .. ' entries on both ends for ' .. name .. "'s " .. professionName)
+                end
+            end
+        end
+    end
+end
+
+local function registerAddonUser(name)
+    if tContains(addonUsers, name) then return end
+    tinsert(addonUsers, name)
+    sort(addonUsers)
+    WhoCanDo:DebugLog('New addon user ' .. name)
+end
+
+local function unregisterAddonUser(name)
+    if not tContains(addonUsers, name) then return end
+    for i = 1, #addonUsers do
+        if addonUsers[i] == name then
+            tremove(addonUsers, i)
+            if bossName == name then
+                waitAndSnatchBoss()
+            end
+            return
+        end
+    end
+end
+
+local function waitAndSnatchBoss(missedMsg)
+    waitingForBoss = true
+    local delay = 0
+    for _, name in ipairs(addonUsers) do
+        if name == playerName then break end
+        delay = delay + 1.6 + fastrandom(30) / 10
+    end
+    C_Timer.After(delay, function()
+        if waitingForBoss then
+            sendImBoss()
+            if missedMsg then WhoCanDo:ProcessGuildMsg(missedMsg, 'missed') end
+        end
+    end)
+end
+
+local function onCommReceived(prefix, msg, channel, sender)
+    if sender == playerName then return end
+
+    local proto, type, data = strsplit(DELIMITER, msg)
+
+    if proto < PROTOCOL_VERSION then
+        if not outOfDatMentioned then WhoCanDo:Log('You are using old version of WhoCanDo, consider updating') end
+        outOfDatMentioned = true
+        return
+    end
+    WhoCanDo:DebugLog(sender .. ' sent ' .. type .. ' ' .. data)
+    if type == MSG_HELLO then
+        if imBoss then sendImBoss() end
+        registerAddonUser(sender)
+        sendHelloBack(sender)
+        compareAndSync(sender, data)
+    elseif type == MSG_HELLOBACK then
+        registerAddonUser(sender)
+    elseif type == MSG_IM_BOSS then
+        imBoss = false
+        waitingForBoss = false
+        bossName = data
+    elseif type == MSG_REQUEST then
+        local profId, name = strsplit(',', data)
+        local professionName = professionIdToName[profId]
+        enqueueEvent({type = MSG_RECIPES, name = name, profession = professionName, target = sender})
+    elseif type == MSG_RECIPES then
+        updateRecipesFromMessage(data)
+    elseif type == MSG_BYE then
+        unregisterAddonUser(sender)
+        if sender == bossName then waitAndSnatchBoss() end
+    end
+end
+
+local function processQueue()
+    if InCombatLockdown() then return end
+    local event = popNextEvent()
+    if not event then return end
+    if event.type == MSG_RECIPES then
+        sendRecipes(event.target, event.profession, event.name)
+    elseif event.type == MSG_REQUEST then
+        sendRequest(event.target, event.profession, event.name)
+    end
+end
+
+function WhoCanDo:AmBoss() return imBoss end
+
+function WhoCanDo:InitializeSync()
+    meta = WCDMetaData
+    deflate = LibStub:GetLibrary("LibDeflate")
+    aceComm = LibStub:GetLibrary("AceComm-3.0")
+    aceComm:RegisterComm(PREFIX, onCommReceived)
+
+    C_ChatInfo.RegisterAddonMessagePrefix(PREFIX)
+    GuildRoster()
+    C_Timer.After(5, function() sendHello() end)
+    C_Timer.After(10, function() if waitingForBoss then waitAndSnatchBoss() end end)
+
+    C_Timer.NewTicker(UPDATE_INTERVAL, processQueue)
+end
+
+function WhoCanDo:CheckOnBossReply(msg)
+    waitingForBoss = true
+    C_Timer.After(BOSS_TTL, function() if waitingForBoss then waitAndSnatchBoss(msg) end end)
+end
+
+function WhoCanDo:OnWCDMessageDetected() waitingForBoss = false end
+
+function WhoCanDo:AnnounceNewData(professionName, name)
+    enqueueEvent({type = MSG_RECIPES, name = name, profession = professionName, target = 'GUILD'})
+end
+
+function WhoCanDo:OnGuildMemberLoggedOff(name)
+    unregisterAddonUser(name)
+end
+
+function WhoCanDo:OnLogout()
+    local msg = buildMessage(MSG_BYE, playerName)
+    aceComm:SendCommMessage(PREFIX, msg, 'GUILD', nil, 'ALERT')
+end
+
+function WhoCanDo:GetAddonUsers() return addonUsers end
---
+++ WhoCanDo.lua
@@ -0,0 +1,392 @@
+WhoCanDo = {Database = {}}
+
+SLASH_WCD1 = '/wcd'
+
+WCDData = {}
+WCDMetaData = {}
+WCDPreferences = {debug = false}
+
+local tradeSkillShown = false
+local onlineGuildies = {}
+WhoCanDo.Guildies = {}
+
+local gameIdToProfessionName = {
+    ['171'] = 'Alchemy',
+    ['164'] = 'Blacksmithing',
+    ['185'] = 'Cooking',
+    ['333'] = 'Enchanting',
+    ['202'] = 'Engineering',
+    ['773'] = 'Inscription',
+    ['755'] = 'Jewelcrafting',
+    ['165'] = 'Leatherworking',
+    ['197'] = 'Tailoring'
+}
+
+local function capitalize(text) return strupper(strsub(text, 1, 1)) .. strlower(strsub(text, 2)) end
+
+local function debugLog(...) if WCDPreferences.debug then print("|cffff6633[|cffffffffWCD|cffff6633]|r", ...) end end
+
+local function log(...) print("|cff66aa33[|cffffffffWCD|cff66aa33]|r", ...) end
+
+function WhoCanDo:Log(...) log(...) end
+
+function WhoCanDo:DebugLog(...) debugLog(...) end
+
+local function initialize()
+    log('WhoCanDo is ready! Type "/wcd" for help')
+    WhoCanDo:InitializeSync()
+    GuildRoster()
+    C_Timer.NewTicker(2 * 60, function() GuildRoster() end)
+
+end
+
+function WhoCanDo:AddRecipe(professionName, spellId, name)
+    local database = WhoCanDo.Database[professionName]
+    if database then
+        if database[spellId] then
+            WCDData[spellId] = WCDData[spellId] or {}
+            local spellDb = WCDData[spellId]
+            if not tContains(spellDb, name) then
+                table.insert(spellDb, name)
+                WCDMetaData[professionName] = WCDMetaData[professionName] or {}
+                WCDMetaData[professionName][name] = WCDMetaData[professionName][name] or 0
+                WCDMetaData[professionName][name] = WCDMetaData[professionName][name] + 1
+                return true
+            end
+        end
+    else
+        debugLog('Unkown profession ' .. professionName)
+    end
+    return false
+end
+
+local function onTradeSkillUpdate()
+    local isLinked, name = IsTradeSkillLinked()
+    if not name then name = UnitName('player') end
+    if not tContains(WhoCanDo.Guildies, name) then
+        debugLog(name, 'is not in the same guild, skipping')
+        return
+    end
+    local numSkills = GetNumTradeSkills()
+    debugLog(numSkills .. ' skills')
+    local listLink = GetTradeSkillListLink()
+    if not listLink then return end
+    local _, _, _, idAndStuff = strsplit(':', listLink)
+    local gameProfId = strsub(idAndStuff, 1, 3)
+
+    local professionName = gameIdToProfessionName[gameProfId]
+    if not professionName then
+        debugLog('Unknown profession id ', gameProfId)
+        return
+    end
+    if not WhoCanDo.Database[professionName] then
+        debugLog(professionName .. ' not supported')
+        return
+    end
+    debugLog('Scanning ' .. name .. "'s " .. professionName)
+    local added = 0
+    for i = 1, numSkills do
+        local spellLink = GetTradeSkillRecipeLink(i)
+        local skillName, skillType = GetTradeSkillInfo(i)
+        if skillType ~= 'header' then
+            local spellId = tonumber(strsub(spellLink, 21, 25))
+            if not spellId then spellId = strsub(spellLink, 21, 24) end
+            local isNew = WhoCanDo:AddRecipe(professionName, spellId, name)
+            if isNew then added = added + 1 end
+        end
+    end
+    if added > 0 then
+        log('Added ' .. added .. ' new recipes for ' .. name .. '.. (' .. WCDMetaData[professionName][name] .. ' total)')
+        WhoCanDo:AnnounceNewData(professionName, name)
+    else
+        debugLog('Nothing new here')
+    end
+end
+
+local tsUpdateId = 0
+local function debounceSkillUpdate()
+    tsUpdateId = tsUpdateId + 1
+    local currentId = tsUpdateId
+    C_Timer.After(0.5, function() if currentId == tsUpdateId then onTradeSkillUpdate() end end)
+end
+
+local function exportCSV(professionName)
+    professionName = capitalize(professionName)
+    if not WhoCanDo.Database[professionName] then
+        log('Unsupported profession ' .. professionName)
+        return
+    end
+
+    for spellId, _ in pairs(WhoCanDo.Database[professionName]) do GetSpellInfo(spellId) end
+
+    C_Timer.After(1, function()
+        local spellList = {}
+        for spellId, _ in pairs(WhoCanDo.Database[professionName]) do
+            local name = GetSpellInfo(spellId)
+            tinsert(spellList, {name = name, spellId = spellId})
+        end
+        table.sort(spellList, function(a, b) return a.name < b.name end)
+
+        local result = {}
+        for _, spell in ipairs(spellList) do
+            tinsert(result, spell.name .. ',' .. table.concat(WCDData[spell.spellId] or {}, ','))
+        end
+
+        local text = table.concat(result, '\n')
+
+        WhoCanDo_ExportFrame:Show()
+        WhoCanDo_EditBox:SetText(text)
+        WhoCanDo_EditBox:HighlightText(0, string.len(text))
+    end)
+end
+
+local function forgetPlayer(name)
+    local removed = 0
+    name = capitalize(name)
+    for _, data in pairs(WCDData) do
+        if tContains(data, name) then
+            for i = 1, #data do
+                if data[i] == name then
+                    tremove(data, i)
+                    removed = removed + 1
+                end
+            end
+        end
+    end
+    WhoCanDo:RecalculateMetaData()
+    if removed > 0 then
+        log('Removed ' .. removed .. ' recipes of ' .. name)
+    else
+        log('Found no recipes for player named ' .. name)
+    end
+end
+
+local function forgetPlayerProfession(name, professionName)
+    local removed = 0
+    name = capitalize(name)
+    professionName = capitalize(professionName)
+    if not WhoCanDo.Database[professionName] then
+        log('Unsupported profession ' .. professionName)
+        return
+    end
+    for spellId, _ in pairs(WhoCanDo.Database[professionName]) do
+        local data = WCDData[spellId] or {}
+        if tContains(data, name) then
+            for i = 1, #data do
+                if data[i] == name then
+                    tremove(data, i)
+                    removed = removed + 1
+                end
+            end
+        end
+    end
+    WhoCanDo:RecalculateMetaData()
+    if removed > 0 then
+        log('Removed ' .. removed .. ' recipes of ' .. name .. "'s " .. professionName)
+    else
+        log('Found no ' .. professionName .. ' recipes for player named ' .. name)
+    end
+end
+
+local function findSpellIdByItemId(itemId)
+    debugLog('Looking for item ' .. itemId)
+    for profession, db in pairs(WhoCanDo.Database) do
+        debugLog('Looking in ' .. profession)
+        for spellId, spellData in pairs(db) do if spellData.item == itemId then return spellId end end
+    end
+    return nil
+end
+
+local function findSpellIdByItemName(name)
+    name = strlower(name)
+    debugLog('Looking for item named ' .. name)
+    for profession, db in pairs(WhoCanDo.Database) do
+        debugLog('Looking in ' .. profession)
+        for spellId, spellData in pairs(db) do
+            local first, last = strfind(spellData.name, name)
+            if first and last then return spellId end
+        end
+    end
+    return nil
+end
+
+local function sendGuildMessage(msg) SendChatMessage('WhoCanDo ' .. msg, "GUILD") end
+
+function WhoCanDo:ProcessGuildMsg(...)
+    local msg, sender = ...
+
+    if not WhoCanDo:AmBoss() and strsub(msg, 1, 8) == 'WhoCanDo' then WhoCanDo:OnWCDMessageDetected(sender) end
+
+    if strsub(msg, 1, 4) == 'wcd ' then
+        if not WhoCanDo:AmBoss() then
+            debugLog("I'm not boss")
+            WhoCanDo:CheckOnBossReply(msg)
+            return
+        end
+        local query = strsub(msg, 5, 500)
+
+        if strlower(query) == 'copium' then
+            sendGuildMessage('Copium©? ♥Alice♥')
+            return
+        end
+
+        local spellId = tonumber(strsub(query, 21, 25)) or tonumber(strsub(query, 21, 24))
+
+        local itemId = tonumber(strsub(query, 18, 22)) or tonumber(strsub(query, 18, 23))
+        if itemId then
+            debugLog('Is ItemId')
+            spellId = findSpellIdByItemId(itemId)
+        elseif not spellId then
+            spellId = findSpellIdByItemName(query)
+        end
+
+        if not spellId then
+            sendGuildMessage("couldn't find " .. query .. ' :(')
+            return
+        end
+
+        local spellLink = GetSpellLink(spellId)
+
+        local crafters = WCDData[spellId]
+
+        if not crafters then
+            sendGuildMessage(spellLink .. '? No one in the guild :(')
+            return
+        end
+
+        local onlineCrafters = {}
+        local offlineCrafters = {}
+
+        for _, name in ipairs(crafters) do
+            if tContains(onlineGuildies, name) then
+                table.insert(onlineCrafters, name)
+            else
+                table.insert(offlineCrafters, name)
+            end
+        end
+
+        sendGuildMessage(spellLink .. '? ♥' .. table.concat(onlineCrafters, ' ') .. ' • ' ..
+                             table.concat(offlineCrafters, ' '))
+    end
+end
+
+local function processGuildPlayers()
+    if not GetGuildInfo('player') then return end
+
+    local membersNum = GetNumGuildMembers()
+
+    if membersNum == nil or membersNum == 0 then return end
+
+    newOnlineGuildies = {}
+    WhoCanDo.Guildies = {}
+
+    for i = 1, membersNum do
+        local fullName, rank, rankIndex, level, class, zone, note, officernote, online = GetGuildRosterInfo(i)
+        local name = strsplit('-', fullName)
+        table.insert(WhoCanDo.Guildies, name)
+        if online then
+            table.insert(newOnlineGuildies, name)
+        elseif tContains(onlineGuildies, name) then
+            WhoCanDo:OnGuildMemberLoggedOff(name)
+        end
+    end
+
+    onlineGuildies = newOnlineGuildies
+end
+
+local function processEvent(event, type, ...)
+    if type == 'VARIABLES_LOADED' then
+        initialize()
+    elseif type == 'TRADE_SKILL_SHOW' then
+        tradeSkillShown = true
+    elseif type == 'TRADE_SKILL_CLOSE' then
+        tradeSkillShown = false
+    elseif type == 'TRADE_SKILL_UPDATE' then
+        debounceSkillUpdate()
+    elseif type == 'CHAT_MSG_GUILD' then
+        WhoCanDo:ProcessGuildMsg(...)
+    elseif type == 'GUILD_ROSTER_UPDATE' then
+        processGuildPlayers()
+    elseif type == 'PLAYER_LOGOUT' then
+        WhoCanDo:OnLogout()
+    end
+end
+
+function WhoCanDo:Start()
+    WhoCanDoEventFrame:RegisterEvent('VARIABLES_LOADED')
+    WhoCanDoEventFrame:RegisterEvent('TRADE_SKILL_SHOW')
+    WhoCanDoEventFrame:RegisterEvent('TRADE_SKILL_UPDATE')
+    WhoCanDoEventFrame:RegisterEvent('TRADE_SKILL_CLOSE')
+    WhoCanDoEventFrame:RegisterEvent('CHAT_MSG_GUILD')
+    WhoCanDoEventFrame:RegisterEvent('CHAT_MSG_ADDON')
+    WhoCanDoEventFrame:RegisterEvent('GUILD_ROSTER_UPDATE')
+    WhoCanDoEventFrame:RegisterEvent('PLAYER_LOGOUT')
+    WhoCanDoEventFrame:SetScript('OnEvent', processEvent)
+end
+
+function WhoCanDo:RecalculateMetaData(loud)
+    local logFunc = loud and log or debugLog
+    logFunc('Recalculating meta data...')
+    for professionName, db in pairs(WhoCanDo.Database) do
+        local newMeta = {}
+        for spellId, _ in pairs(db) do
+            if WCDData[spellId] then
+                for i, name in ipairs(WCDData[spellId]) do newMeta[name] = (newMeta[name] or 0) + 1 end
+            end
+        end
+        WCDMetaData[professionName] = newMeta
+    end
+    logFunc('Meta data recalculated!')
+end
+
+function SlashCmdList.WCD(rawCmd)
+    local cmd, arg1, arg2 = strsplit(' ', rawCmd)
+    cmd = strlower(cmd)
+    if cmd == 'forget' then
+        if arg1 == 'all' then
+            WCDData = {}
+            WCDMetaData = {}
+            log('All stored data has been reset')
+        elseif arg1 then
+            if arg2 then
+                forgetPlayerProfession(arg1, arg2)
+            else
+                forgetPlayer(arg1)
+            end
+        else
+            log('Forget what?')
+            print('Examples:')
+            local guildies = WhoCanDo.Guildies
+            local gIndex1 = fastrandom(1, #guildies)
+            local gIndex2 = fastrandom(1, #guildies)
+            print('/wcd forget ' .. guildies[gIndex1])
+            print('/wcd forget ' .. guildies[gIndex2] .. ' tailoring')
+            print('/wcd forget all')
+        end
+    elseif cmd == 'recalculate' then
+        WhoCanDo:RecalculateMetaData(true)
+    elseif cmd == 'who' then
+        log('Current users: ' .. table.concat(WhoCanDo:GetAddonUsers(), ', '))
+    elseif cmd == 'export' then
+        if not arg1 then
+            log('Usage: /wcd export [profession name], e.g.: /wcd export jewelcrafting')
+            return
+        end
+        exportCSV(arg1)
+    elseif not cmd or cmd == '' or cmd == 'help' then
+        log('Ask a question in /guild chat in format: "wcd glyph of bladestorm"')
+        print('Other supported commands:')
+        print(' To export into csv/sheet:')
+        print('|cffcccccc    /wcd export |cff999999[profession name]')
+        print(' To remove data:')
+        print('|cffcccccc    /wcd forget |cff999999[player name]')
+        print('|cffcccccc    /wcd forget |cff999999[player name] [profession name]')
+        print('|cffcccccc    /wcd forget all')
+        print(' To show who is using the addon:')
+        print('|cffcccccc    /wcd who')
+    else
+        log('Unknown command /wcd ' .. cmd .. ', try /wcd help')
+    end
+end
+
+WhoCanDo:Start()
